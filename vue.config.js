59e9c62df65c2ca35103f499c31299386cd55265
Marian <hevein@live.it>
2022-12-27 10:25:42 +0100

    Pages Test

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-    devServer: {
-        host: '127.0.0.1',
-        port: 8080,
-    },
+  devServer: {
+    host: "127.0.0.1",
+    port: 8080,
+  },
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+
+  publicPath: process.env.NODE_ENV === "production" ? "/EvolveDex/" : "/",
+});
